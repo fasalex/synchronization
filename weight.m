055a60939b658f6285764a7d034df094eb1226e8
fasalex <fasalex@6fd8a465-204b-0410-afcd-1399405c8cbe>
2008-04-21 12:15:47 +0000

    New algorithm in MacLayer.cc and sync.m

--- weight.m
+++ weight.m
@@ -6,23 +6,34 @@ pos = y(find( y>=0 )) ;
 neg = y(find( y<0 ));
 
 ######### Compare the values of the timing
-
-Median = median(y) ;
-Mean = mean(y) ;
-
 len = length(y) ;
 len_pos = length(pos) ;
 len_neg = length(neg) ;
+Median = median(y) ;
+
+Mean = mean(y) ;
+diff = abs(y - Median);
+diff_sum = sum(diff) ;
+if(len != 1)&&(diff_sum !=0 ) 
+wei = (1 - diff ./ diff_sum) ./ (len-1);
+else 
+wei = 1 ;
+endif 
+
+yy = wei.*y ;
+WeightyP = sum(yy) ;
+Weighty = WeightyP ;
+
 %[Mean Median] = calculate_weight(pos,neg);
 
 ######### Reference value
  
-for(h=1:len_neg)
-neg(h) = neg(h) + (max_drift/len_neg)* h ;
-endfor 
-for(t=1:len_pos)
-pos(t) = pos(t) - (max_drift/len_pos)* (t-1) ;
-endfor 
-[Weighty WeightyP] = calculate_weight(pos,neg);
+######### for(h=1:len_neg)
+######### neg(h) = neg(h) + (max_drift/len_neg)* h ;
+######### endfor 
+######### for(t=1:len_pos)
+######### pos(t) = pos(t) - (max_drift/len_pos)* (t-1) ;
+######### endfor 
+######### [Weighty WeightyP] = calculate_weight(pos,neg);
 
 
