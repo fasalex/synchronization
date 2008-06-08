479c2ac7353cf56f211be8b525c03f1087009b40
fasalex <fasalex@6fd8a465-204b-0410-afcd-1399405c8cbe>
2008-06-08 18:09:55 +0000

    Weekend update on the algorithms

--- trunk/MacLayer.cc
+++ trunk/MacLayer.cc
@@ -6,208 +6,276 @@ Define_Module(MacLayer);
 //---intialisation---------------------
 
 void MacLayer::initialize(int stage) {
-	
-	hostCount = parentModule()->par("hosts");
-	periodCount = parentModule()->par("limit");
-	algorithm  = parentModule()->par("algorithm");
-	gain = parentModule() ->par("gain");
-	jump = parentModule() ->par("jump") ;
-	BaseModule::initialize(stage);
-        
-	if(stage == 0) {
-		myIndex = parentModule()->par("id") ;
-		Period = 0 ;
-		count = 0;
-		Period = 0 ;
-		Ref = 0 ;
-		dataOut = findGate("lowerGateOut");
-		dataIn = findGate("lowerGateIn");
-		controlOut = findGate("lowerControlOut");
-		controlIn = findGate("lowerControlIn");
-		random_start = (double) parentModule()->par("start_time") * 29/327868 ;
-		double t = parentModule()->par("start_time"); 
-		frequency = clock_const + clock_const*0.001*0.06*(0.5-t) ;
-		ev << frequency*1000000 << endl ;
-	} 
-	else if(stage == 1) {
-		broadcast_time = random_start*10;
-		MacPkt* pkt = createMacPkt(frame_length);
-		scheduleAt(broadcast_time, pkt);
+       
+       hostCount = parentModule()->par("hosts");
+       algorithm  = parentModule()->par("algorithm");
+       gain = parentModule() ->par("gain");
+       jump = parentModule() ->par("jump") ;
+       BaseModule::initialize(stage);
+       
+       if(stage == 0) {
+               myIndex = parentModule()->par("id") ;
+               Period = 0 ;
+               count = 0;
+               Period = 0 ;
+               Ref = 0 ;
+               dataOut = findGate("lowerGateOut");
+               dataIn = findGate("lowerGateIn");
+               controlOut = findGate("lowerControlOut");
+               controlIn = findGate("lowerControlIn");
+	       double rnd = parentModule()->par("start_time") ;
+               random_start = (double) parentModule()->par("start_time") * 29/32768 ;
+               double t = parentModule()->par("start_time");
+               frequency = clock_const + clock_const*30*1e-6*(rnd-0.5);  
+               ev << frequency*1000000 << endl ;
+       }
+       else if(stage == 1) {
+               broadcast_time = random_start;
+               MacPkt* pkt = createMacPkt(frame_length);
+               scheduleAt(broadcast_time, pkt);
 
-		cMessage *ctrl = new cMessage("Control Message");
-		Ref = broadcast_time + 0.5*frequency ;
-		Period++ ;
-		ctrl->setKind(CONTROL_MESSAGE);
-		scheduleAt(Ref,ctrl);
-	}
+               cMessage *ctrl = new cMessage("Control Message");
+               Ref = broadcast_time + 0.5*frequency ;
+               Period++ ;
+               ctrl->setKind(CONTROL_MESSAGE);
+               scheduleAt(Ref,ctrl);
+       }
 }
 
 void MacLayer::handleMessage(cMessage* msg) {
-	if( msg->kind() == BROADCAST_MESSAGE ){
-		logg("Sending Broadcast Messages to the physical Layer ....");
-		output_vec.record(broadcast_time*1000000) ;
-		sendDown(msg);
-	}
-	else if ( msg->kind() == CONTROL_MESSAGE){
-		logg("Control Message Received - Updating the period ...") ;
-		analyze_msg();
-		delete msg;
-	}else{	
-		logg("Collecting the offsets from Neighbours ....");
-		if((simTime()<500)&&(dblrand() < 0.4)){}else
-		collect_data(msg);
-		delete msg ;
-	}
+       if( msg->kind() == BROADCAST_MESSAGE ){
+               logg("Sending Broadcast Messages to the physical Layer ....");
+               output_vec.record(broadcast_time*1000000) ;
+               sendDown(msg);
+       }
+       else if ( msg->kind() == CONTROL_MESSAGE){
+               logg("Control Message Received - Updating the period ...") ;
+               analyze_msg();
+               delete msg;
+       }else{  
+               logg("Collecting the offsets from Neighbours ....");
+//             if((simTime()<500)&&(dblrand() < 0.4)){}else
+               collect_data(msg);
+               delete msg ;
+       }
 }
 void MacLayer::sendDown(cMessage *pkt)
 {
-	send(pkt, dataOut);
+       send(pkt, dataOut);
 }
 
 void MacLayer::collect_data(cMessage *pkt)
-{	
-	clock_drift = (temperature - 25 )* exp(-6) * 0.5 / 20;
-	clock_drift = 0 ;
-	temp_varr[count] = broadcast_time - simTime() +  clock_drift;
-	count++;
-	logg("Recording the simulation values");
+{      
+       clock_drift = (temperature - 25 )* exp(-6) * 0.5 / 20;
+       clock_drift = 0 ;
+       temp_varr[count] = broadcast_time - simTime() +  clock_drift;
+       count++;
+       logg("Recording the simulation values");
 }
 
 void MacLayer::analyze_msg()
-{	
-	logg("Adjusting the offset of ") ;
+{      
+       logg("Adjusting the offset of ") ;
 
-	int neigh = count;
-	double total = 0 ;
-	double offset = 0 ;
+       int neigh = count;
+       double total = 0 ;
+       double offset = 0 ;
 
-	for(int x = 0; x < neigh; x ++) {
-		for(int y = x+1; y < neigh; y ++) {
-     				if(temp_varr[y] < temp_varr[x]) {
-               				double temp = temp_varr[x];
-               				temp_varr[x] = temp_varr[y];
-               				temp_varr[y] = temp;
-				}
+       for(int x = 0; x < neigh; x ++) {
+               for(int y = x+1; y < neigh; y ++) {
+                               if(temp_varr[y] < temp_varr[x]) {
+                                       double temp = temp_varr[x];
+                                       temp_varr[x] = temp_varr[y];
+                                       temp_varr[y] = temp;
+                               }
+               }
+               total = total + temp_varr[x] ;
+       }
+       
+       int medium_value = (int) neigh / 2 ;
+       double median;
+       double add_on ;
+       add_on = 0 ;
+
+       for(int k=0;k<neigh;k++)
+	 temp_varr[k] = temp_varr[k] - add_on ;
+
+       if(neigh ==0)
+               median = 0 ;
+       else if ( neigh == 1)
+               median = temp_varr[neigh] ;
+       else
+               median = 0.5 *(temp_varr[medium_value - 1] + temp_varr[medium_value]) ;
+
+       switch(algorithm){
+
+// Weighted Measurments ............
+
+       case 1:{ double maxx  ;
+                double fasika ;
+                double weight[SIZE_OF_NETWORK];
+                double tempp[SIZE_OF_NETWORK] ;
+                double sum = 0 ;
+
+                for(int af = 0 ; af < neigh ; af ++) {
+                        tempp[af] = abs(temp_varr[af]-median);
+                        sum = sum + tempp[af] ;
+                }
+                if(tempp[0] >= tempp[neigh-1])
+                maxx = tempp[0] ;
+                else 
+                maxx = tempp[neigh-1] ;
+
+                if(sum==0)
+                        offset = 0 ;
+                else{
+                fasika = (neigh * sum) - sum ;
+                for (int m=0; m < neigh; m++){
+			if(fasika !=0)
+                         weight[m] = (sum - tempp[m]) / fasika ;
+			else 
+			 weight[m] = 0 ;
+                         offset = offset + (temp_varr[m] * weight[m]) ;
+                }
+                offset = offset*gain ;
+                }
+                break;}
+       case 2:{
+
+// Median .........with a gain 
+               offset = median ;
+               break;}
+
+       case 3:{
+
+// Kalman filter .....
+
+               	double x; // estimated value of the variable to be considered 
+		double phi; // coefficient to bla bla the previous estimate in it
+		double z;   // measured value
+		double H;  // adjustment matrix
+		double R;  // noise covariance
+		double P;  // error covariance 
+		double Ka;  // Kalman gain 
+		double Q;  // noise covariance  
+		double B;  // control coefficient 
+		double U;  // control factor 
+		// Initialize the matrices 
+
+		x = 1 ; // Initial estimate 
+		P = 1 ; // Initial estimate of covariance matrix - error covariance matrix
+
+		phi = 1 ;
+		Q = 1 ;
+		R = 1 ;
+		H = 1 ;
+		B = 0 ;
+		// Loop 
+		for(int i=0;i<neigh;i++){
+		// Time update "PREDICT"
+
+		x = phi*x + B*U ;
+		P = (phi * P) * (1/phi) + Q ;
+
+		// Measurment Update "CORRECT"
+
+		// Compute Kalman gain
+
+		Ka = P * H ; // ( ( H * P * H ) + R);	
+
+		// update estimate with measurement
+
+		x = x + Ka * (z - (H * x) );
+
+		// update the error covariance
+
+		P = ( 1 - (Ka * H)) * P;	
+
+		//endloop 
 		}
-                total = total + temp_varr[x] ;
-        }
-	
-	int medium_value = (int) neigh / 2 ;
-	double median;
-	if(neigh ==0)
-		median = 0 ;
-	else if ( neigh == 1)
-		median = temp_varr[neigh] ;
-	else
-	 	median = 0.5 *(temp_varr[medium_value - 1] + temp_varr[medium_value]) ;
+		offset = x *1e-6 ;
+		offset = 1e-6;
+               break;}
+       case 4:{
 
-	switch(algorithm){
-	case 1:{
-		double average ;
-		if(neigh != 0)
-		average = total / neigh ;
-		else
-		average = 0 ;
-		offset = average ;
-		break;}
-	case 2:{
-		offset = median ;
-		break;}
-	case 3:{
-		double maxx  ;
-		double fasika ;
-		double weight[SIZE_OF_NETWORK];
-		double tempp[SIZE_OF_NETWORK] ;
-		double sum = 0 ;
-		temp_varr[neigh] = 0 ;
-		for(int af = 0 ; af < neigh+1 ; af ++) {
-			tempp[af] = abs(temp_varr[af]-median);
-			sum = sum + tempp[af] ;
-		}
-		if(tempp[0] >= tempp[neigh-1])
-		maxx = tempp[0] ;
-		else 
-		maxx = tempp[neigh-1] ;
+// Curve fitting - logartithmic 
 
-		if(sum==0)
-			offset = 0 ;
-		else{
-                fasika = ((neigh+1) * maxx) - sum ;
-		for (int m=0; m < neigh+1; m++){
-		         weight[m]= (maxx - tempp[m]) / fasika ;
-			 offset = offset + (temp_varr[m] * weight[m]) ;
-		}
-		offset = offset * gain;
- 		}
-		break;}
-	case 4:{
-		double a,b,sum,sumsq,sumprod,sumprodsum = 0;
-		int varr = 1 ;
-		bool change = false ;	
-		
-		for(int k=0 ; k<count ; k++){
-			if((temp_varr[k] >= 0) && (change == false)){
-				varr = k+1 ;
-				change = true;
-			}
-			sum += log(k+1);
-			sumsq += pow(log(k+1),2);
-			sumprod += temp_varr[k] * log(k+1);	
-		}
+               double a,b,sum,sumsq,sumprod,sumprodsum = 0;
+               int varr = 1 ;
+               bool change = false ;  
+               
+               for(int k=0 ; k<count ; k++){
+                       if((temp_varr[k] >= 0) && (change == false)){
+                               varr = k+1 ;
+                               change = true;
+                       }
+                       sum += log(k+1);
+                       sumsq += pow(log(k+1),2);
+                       sumprod += temp_varr[k] * log(k+1);    
+               }
 
-		for(int k=0;k<count;k++){
-			sumprodsum += temp_varr[k]*sum ;
-		}
+               for(int k=0;k<count;k++){
+                       sumprodsum += temp_varr[k]*sum ;
+               }
 
-		b = (count*sumprod - sumprodsum)/(count*sumsq - (sum*sum));
-		a = (total - b*sum) / count ;
-		varr = abs(count/2 - varr) ;
-		offset = a + b*log(varr)  ;
-	}
-	default:
-		offset = 0;
-		break;
-	}
+               b = (count*sumprod - sumprodsum)/(count*sumsq - (sum*sum));
+               a = (total - b*sum) / count ;
+               varr = abs(count/2 - varr) ;
+               offset = a + b*log(varr)  ;
+               offset = 1e-6 ;
+       }
+       case 5:{
+// MMSE estimator ....
 
-	if(Period%jump != 0)
 		offset = 0 ;
-	if(offset > (0.5*frequency/gain)) 
-		offset = 0 ;
+	       break;
 
-	broadcast_time = broadcast_time - gain*offset + frequency ;
-	Ref = Ref - gain*offset + frequency ;
+	}
+       default:
+               offset = 0;
+               break;
+       }
+//	offset = 0 ;
+       if(Period%jump != 0)
+               offset = 0 ;
+//      if(offset > (0.5*frequency/gain))  offset = 0 ;
+       offset = offset + add_on ;
+       broadcast_time = broadcast_time - gain*offset + frequency ;
+       Ref = Ref - gain*offset + frequency ;
 
-        MacPkt* pkt = createMacPkt(frame_length);
-	scheduleAt(broadcast_time,pkt) ;
+       MacPkt* pkt = createMacPkt(frame_length);
+       scheduleAt(broadcast_time,pkt) ;
 
-	cMessage *ctrl = new cMessage("Control Message");
-	Period++;
-	ctrl->setKind(CONTROL_MESSAGE);
-	scheduleAt(Ref,ctrl);
+       cMessage *ctrl = new cMessage("Control Message");
+       Period++;
+       ctrl->setKind(CONTROL_MESSAGE);
+       scheduleAt(Ref,ctrl);
 
-	for(int k=0;k<count;k++)
-		temp_varr[count] = 0 ;
-	
- 	count = 0 ;
+       for(int k=0;k<count;k++)
+               temp_varr[count] = 0 ;
+       
+       count = 0 ;
 
 }
 
 void MacLayer::finish()
 {
-	recordScalar("Time at last", broadcast_time);
+       recordScalar("Time at last", broadcast_time);
 }
 
 void MacLayer::logg(std::string msg)
 {
-	ev << "[Node " << myIndex << "] - MacLayer: " << msg << endl;
+       ev << "[Node " << myIndex << "] - MacLayer: " << msg << endl;
 }
 
+
 MacPkt* MacLayer::createMacPkt(simtime_t length) {
-	Signal* s = new Signal(broadcast_time, length);
-	MacToPhyControlInfo* ctrl = new MacToPhyControlInfo(s);
-	MacPkt* res = new MacPkt();
-	res->setControlInfo(ctrl);
-	res->setKind(BROADCAST_MESSAGE);
-	res->setDestAddr(nextReceiver);
-	return res;
+       Signal* s = new Signal(broadcast_time, length);
+       MacToPhyControlInfo* ctrl = new MacToPhyControlInfo(s);
+       MacPkt* res = new MacPkt();
+       res->setControlInfo(ctrl);
+       res->setKind(BROADCAST_MESSAGE);
+       res->setDestAddr(nextReceiver);
+       return res;
 }
+
--- trunk/simulate.m
+++ trunk/simulate.m
@@ -1,15 +1,15 @@
-clf;
+clf ;
 system("rm *.vec");
 system("rm *.sca");
 
 number_of_nodes = 16 ;
 rnd = rand(1,number_of_nodes) ;
 hold on ;
-for fasika = 1:5
+
+for fasika = 1:2
+
 keep('fasika','number_of_nodes','rnd');
-algorithm = 2  ;
-gain = 0.75
-speed = fasika * 20 -20  ;
+algorithm = fasika  ;
 
 if (fasika == 1)
 color = 'b' ;
@@ -19,27 +19,35 @@ elseif(fasika == 3)
 color = 'g';
 elseif(fasika == 4)
 color = 'm';
-elseif(fasika ==5)
+elseif(fasika == 5)
 color = 'c';
+elseif(fasika == 6)
+color = 'w';
 endif
 
+prefix = "/home/fasika/mixim/trunk/examples/Output/"; % output directory
+model = "./New"; % simulation command
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%          INPUT PARAMETERS FOR THE SIMULATION                         %%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 run = 1 ;                           %% 1 - commandline  2 - gui 
 jump = 1 ;                          %% jump to reduce the calculation burden 
-sim_time_limit = 1000;		    %% Number of events needed for "limit"                        
-updateInterval = 1;                 %% In simulation seconds 
+sim_time_limit = 1000;		    %% Number of events needed for "limit" 
+speed = 10 ;                         %%  In Kilometer per hour 
+updateInterval = 1;              %% In simulation seconds 
+gain = 0.75 ;                       %% Value for computing the offsets 
 express = "yes" ;                   %% Enable or Disable express mode 
 alpha = 2.50 ;                      %% Channel factor - attenuation if you might say ...
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% create ini file
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%          END OF PARAMETERS , OUT                                     %%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-prefix = "/home/fasika/mixim/trunk/examples/Output/"; % output directory
-model = "./New";               % simulation command
+%% create ini file
 
 if(run==1)
 system("cp Makefile_cmd Makefile");
-else 
+else
 system("cp Makefile_tk Makefile");
 endif
 
@@ -56,8 +64,8 @@ alp = int2str(alpha*100);
 ext = '.eps';
 filename = strcat(prefix,'s',spe,'-g',gai,'-n',no,'-alpha',alp,ext);
 
-playgroundSizeX = (sqrt(number_of_nodes) + 2) * 150  ; %% meters 
-playgroundSizeY = (sqrt(number_of_nodes) + 2) * 150  ; %% meters 
+playgroundSizeX = (sqrt(number_of_nodes) + 2) * 150 ; %% meters
+playgroundSizeY = (sqrt(number_of_nodes) + 2) * 150 ; %% meters
 
 fidout = fopen(ininame, "w", "native");
 fprintf(fidout, "[General]\n");
@@ -70,7 +78,7 @@ fprintf(fidout, "express-mode = %s\n", express);
 
 fprintf(fidout, "[Parameters]\n");
 fprintf(fidout, "##############################\n");
-fprintf(fidout, "#			Simulation parameters                        #\n");
+fprintf(fidout, "# Simulation parameters #\n");
 fprintf(fidout, "##############################\n");
 fprintf(fidout, "mobileNet.**.coreDebug = 0\n");
 fprintf(fidout, "mobileNet.playgroundSizeX = %d\n", playgroundSizeX);
@@ -83,13 +91,13 @@ fprintf(fidout, "mobileNet.Node[*].normalNic.gain = %f \n" , gain) ;
 fprintf(fidout, "mobileNet.Node[*].normalNic.hosts = %d \n\n", number_of_nodes);
 
 fprintf(fidout, "##############################\n");
-fprintf(fidout, "#			World Utility Parameters                        #\n");
+fprintf(fidout, "# World Utility Parameters #\n");
 fprintf(fidout, "##############################\n");
 fprintf(fidout, "mobileNet.world.useTorus = 0\n");
 fprintf(fidout, "mobileNet.world.use2D = 0\n\n");
 
 fprintf(fidout, "##############################\n");
-fprintf(fidout, "#			Channel Parameters                        #\n");
+fprintf(fidout, "# Channel Parameters #\n");
 fprintf(fidout, "##############################\n");
 fprintf(fidout, "mobileNet.Channel.coreDebug = 0\n");
 fprintf(fidout, "mobileNet.Channel.sendDirect = 0\n");
@@ -99,12 +107,12 @@ fprintf(fidout, "mobileNet.Channel.alpha = %f\n",alpha);
 fprintf(fidout, "mobileNet.Channel.carrierFrequency = 2.45e+9\n\n");
 
 fprintf(fidout, "##############################\n");
-fprintf(fidout, "#			Host specific Parameters                        #\n");
+fprintf(fidout, "# Host specific Parameters #\n");
 fprintf(fidout, "##############################\n");
 fprintf(fidout, "mobileNet.*Node*.utility.coreDebug = 0\n\n");
 
 fprintf(fidout, "##############################\n");
-fprintf(fidout, "#			Physical Layer Parameters                       #\n");
+fprintf(fidout, "# Physical Layer Parameters #\n");
 fprintf(fidout, "##############################\n");
 fprintf(fidout, "mobileNet.Node[*].normalNic.phy.usePropagationDelay = true\n");
 fprintf(fidout, "mobileNet.Node[*].normalNic.phy.thermalNoise = 1.0\n");
@@ -120,7 +128,7 @@ fprintf(fidout, "mobileNet.Node[*].normalNic.phy.maxTXPower = 20.0\n");
 fprintf(fidout, "mobileNet.Node[*].normalNic.phy.timeSleepToTX = 0.035\n\n");
 
 fprintf(fidout, "##############################\n");
-fprintf(fidout, "#			Host Parameters                        #\n");
+fprintf(fidout, "# Host Parameters #\n");
 fprintf(fidout, "##############################\n");
 fprintf(fidout, "mobileNet.Node[*].normalNic.connectionManagerName = \"Channel\"\n");
 fprintf(fidout, "mobileNet.Node[*].mobility.angle = 2\n");
@@ -146,13 +154,13 @@ endwhile
 j=0;
 endfor
 
-%%% Changing to simulation speed 
+%%% Changing to simulation speed
 speed = speed * (1/3.6) ;
 fprintf(fidout, "mobileNet.Node[*].mobility.speed= %f\n", speed);
 fprintf(fidout, "mobileNet.Node[*].mobility.updateInterval= %f\n", updateInterval);
 fprintf(fidout, "mobileNet.Node[*].mobility.debug = 0 \n\n");
 fclose(fidout) ;
-go = 1;%input("Do you wanna see the plot after simulation ? 1 / 0 ");
+
 %%
 %% run the simulations
 %%
@@ -173,83 +181,84 @@ fclose(fidin);
 system("splitvec omnetpp.vec");
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%  Data extract from the vector file .
+%%%%% Data extract from the vector file .
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 for (i=0:number_of_nodes-1)
-	vec_n = i + vec_num ;
-	vectorname = sprintf("omnetpp-%d.vec", vec_n);
-	fidin = fopen(vectorname, "r", "native");
-	if (fidin==-1)
-  	error("Unable to open the vector file");
-	endif
-	nRows=0;
-	while 1
-    		iString=fgetl(fidin);
-    		if ~ischar(iString)
-       			break;
-    		endif
-    	nRows=nRows+1;
-	endwhile
+vec_n = i + vec_num ;
+vectorname = sprintf("omnetpp-%d.vec", vec_n);
+fidin = fopen(vectorname, "r", "native");
+if (fidin==-1)
+error("Unable to open the vector file");
+endif
+nRows=0;
+while 1
+iString=fgetl(fidin);
+if ~ischar(iString)
+break;
+endif
+nRows=nRows+1;
+endwhile
 
-	%Return to beginning of file
-	fseek(fidin,0,'bof');
+%Return to beginning of file
+fseek(fidin,0,'bof');
 
-	%For each row, assign each space delimitted object to a cell in the "Output" matrix
-	for iRow=1:nRows
-   		iCol=1;
-    	%Temporary storage of the first object
-    	%   Note: the space delimitter used here can be replaced by any delimitter
-    		[TempOutput,Rem]=strtok(fgetl(fidin));
-		
-    	%If there is now data on this row, then assign the first object to be an underscore
-		if (length(TempOutput) == 0)
-        		TempOutput='_';
-    		endif
-    	%Build the "Output" matrix this will be the first column of the iRow-th row
+%For each row, assign each space delimitted object to a cell in the "Output" matrix
+for iRow=1:nRows
+iCol=1;
+%Temporary storage of the first object
+% Note: the space delimitter used here can be replaced by any delimitter
+[TempOutput,Rem]=strtok(fgetl(fidin));
 
-		ascii_output = toascii(TempOutput) ;
+%If there is now data on this row, then assign the first object to be an underscore
+if (length(TempOutput) == 0)
+TempOutput='_';
+endif
+%Build the "Output" matrix this will be the first column of the iRow-th row
 
-		if(ascii_output(1) ==35)
-		Remm = toascii(Rem); 
-		start = find(Remm == 91);
-		eend = find(Remm == 93) ;
-		nodde = substr(Rem, start+1, eend-start-1) ;
-		[node_num coun] = sscanf(nodde , "%d");
-		else
-		num = find (ascii_output == 9) ;
-		TempOutput = substr(TempOutput,num(1)+1,num(2)-num(1));
-		[val,count] = sscanf(TempOutput,"%d");
-		if(iRow > 1)		
-		MainVector(node_num+1,iRow-1) = val;
-		endif
-		endif
-	    	%Repeat this only using Rem as the total string and incrementing the iCol counter
-        endfor
-	fclose(fidin);
-endfor 
 
-%% The vector which contains the time information about the nodes 
+ascii_output = toascii(TempOutput) ;
+
+if(ascii_output(1) ==35)
+Remm = toascii(Rem);
+start = find(Remm == 91);
+eend = find(Remm == 93) ;
+nodde = substr(Rem, start+1, eend-start-1) ;
+[node_num coun] = sscanf(nodde , "%d");
+else
+num = find (ascii_output == 9) ;
+TempOutput = substr(TempOutput,num(1)+1,num(2)-num(1));
+[val,count] = sscanf(TempOutput,"%d");
+if(iRow > 1)
+MainVector(node_num+1,iRow-1) = val;
+endif
+endif
+%Repeat this only using Rem as the total string and incrementing the iCol counter
+endfor
+fclose(fidin);
+endfor
+
+%% The vector which contains the time information about the nodes
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%     Data extracting from the scalar file ....
+%%%% Data extracting from the scalar file ....
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %fidin = fopen(scalarname, "r", "native");
 %k=0;
 %if (fidin==-1)
-%  error("Unable to open the scalar file");
+% error("Unable to open the scalar file");
 %endif
 %while 1
-%    	iString=fgetl(fidin);
-%    	if ~ischar(iString)
-%       		break;
-%   	endif
-%	[val, count] = sscanf(iString, "scalar \"mobileNet.Node[%d].normalNic.mac\" 	\"Time at last\" 	%f");
-%	if( count == 0)
-%	else
-%	final_time(k,k) = val(2);
-%	final(k) = val(2) ;	
-%	endif
-%  	k = k + 1 ;
+% iString=fgetl(fidin);
+% if ~ischar(iString)
+% break;
+% endif
+% [val, count] = sscanf(iString, "scalar \"mobileNet.Node[%d].normalNic.mac\" \"Time at last\" %f");
+% if( count == 0)
+% else
+% final_time(k,k) = val(2);
+% final(k) = val(2) ;
+% endif
+% k = k + 1 ;
 %endwhile
 %fclose(fidin);
 %offset = final_time / max(max(final_time));
@@ -265,15 +274,17 @@ endfor
 %axis([0 1000 0 1000 0 1 ]);
 %endif
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%                   End of Simulation -- Deleting files 
+%%%% End of Simulation -- Deleting files
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-MainVector(:,sim_time_limit+1) = 0 ;
+
+m = length(MainVector);
+MainVector(:,sim_time_limit:m) = 0 ;
+
 plot(std(MainVector/30),color);
 endfor
 xlabel('period(sec)');
-ylabel('Synchronization error(clock cycles)');
-legend("0","20","40","60","80");
-%axis([0 sim_time_limit 0 30]);
+ylabel('synchronization error(clock cycles)');
+legend("Weighted measurment","Median","kalman filter","Curve fitting","Minimum Mean Square Estimator");
 print(filename);
 hold on ;
 disp("SIMULATION ENDED") ;
